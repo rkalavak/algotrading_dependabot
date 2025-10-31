316cb9fa81c9be4e7dbe25c5464146ea70ad02fe
rkalavak <kalavakurirenu@gmail.com>
2025-10-31 20:19:54 +0530

    Initial commit

---
+++ src/main/java/com/kalavakuri/algotrading_dependabot/AlgotradingDependabotApplication.java
@@ -0,0 +1,13 @@
+package com.kalavakuri.algotrading_dependabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgotradingDependabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgotradingDependabotApplication.class, args);
+	}
+
+}
